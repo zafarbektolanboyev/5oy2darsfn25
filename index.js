950c715381c2f70fc04ef6e2f6fddb8269a2a59c
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-06-17 15:50:02 +0500

    htmljs

---
+++ index.js
@@ -0,0 +1,29 @@
+// 1
+// function number(massiv) {
+//     let numLength = massiv.length;
+//     if (numLength < 3) {
+//         return [];
+//     }
+    
+//     let midle = Math.floor(numLength / 2);
+//     return massiv.slice(midle - 1, midle + 2);
+// }
+// console.log(number([1, 2, 3, 4]));
+
+// 2
+// let arrays = [
+//     ['Zafarbek', 'Ahad', 'Jasur'],
+//     [4, 5, 6],
+//     ['olcha', 'gilos', 'olma']
+// ];
+// function swap(arrays) {
+//     let temp = arrays[0];
+//     arrays[0] = arrays[2];
+//     arrays[2] = temp;
+// }
+// swap(arrays);
+// arrays.forEach(arr => {
+//     console.log(arr);
+// });
+
+// 3
