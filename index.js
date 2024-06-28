dfbf87f5d404fad85e0d2645bf9bd53c57d04d01
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-06-28 18:55:47 +0500

    js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1
+//1
 // function number(massiv) {
 //     let numLength = massiv.length;
 //     if (numLength < 3) {
@@ -8,135 +8,113 @@
 //     let midle = Math.floor(numLength / 2);
 //     return massiv.slice(midle - 1, midle + 2);
 // }
-// console.log(number([1, 2, 3, 4]));
-
-// 2
-// let arrays = [
-//     ['Zafarbek', 'Ahad', 'Jasur'],
-//     [4, 5, 6],
-//     ['olcha', 'gilos', 'olma']
-// ];
-// function swap(arrays) {
-//     let temp = arrays[0];
-//     arrays[0] = arrays[2];
-//     arrays[2] = temp;
-// }
-// swap(arrays);
-// arrays.forEach(arr => {
-//     console.log(arr);
-// });
+// console.log(number([1, 2, 3, 4, 5]));
 
 // 3
-// let numbers = [1,2,3,4,5,6,7,8];
-// function lastIndexOf(arr, value) {
-//     for (let i = arr.length - 1; i >= 0; i--) {
-//         if (arr[i] === value) {
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
-// console.log(lastIndexOf(numbers, 5));
-// console.log(lastIndexOf(numbers, 6));
+// const num1 = [1,2,3,4,5,6,7];
+// const num2 = [23,46,29,12];
+// const arr3 = num1.concat(num2)
+// console.log(arr3)
 
 // 4
-// let arr1 = [1,2,3,4,5,6,7];
-// let arr2 = [8,9,4,10,3,1];
-// let value = 1;
-// function find(arr1, arr2, value){
-//     let foundArr1 = arr1.includes(value);
-//     let foundArr2 = arr2.includes(value);
-//     return foundArr1 && foundArr2;
+// function indexOf(array, value) {
+//     let index = array.indexOf(value);
+//     if (index !== -1) {
+//         return Math.pow(index, 7);
+//     } else {
+//         return false;
+//     }
 // }
-// console.log(find(arr1,arr2, value))
+// console.log(indexOf([1, 2, 7, 4], 7));
 
-// 5
-// let fruits = ['gilos', 'olma', 'olcha'];
-// function includesElement(arr, value) {
-//     return arr.includes(value);
+//5
+// let num = [1,2,3,4,5,6,7];
+// function lastIndexOfFind(arr, value){
+//     return arr.lastIndexOf(value)
 // }
-// console.log(includesElement(fruits, 'olma'));
-// console.log(includesElement(fruits, 6));
+// console.log(lastIndexOfFind(num))
 
-//6
-// let num = [123,782,333,657,823];
+// 6
+// function isElementInBothArrays(array1, array2, element) {
+//     return array1.indexOf(element) !== -1 && array2.indexOf(element) !== -1;
+// }
+// console.log(isElementInBothArrays([1, 2, 3], [3, 4, 5], 3));  // true
+// console.log(isElementInBothArrays([1, 2, 3], [4, 5, 6], 3));  // false
+
+// 7
+// let num = [1,2,3,4,5];
+// function isElementFind(arr, value){
+//     return arr.includes(value)
+// }
+// console.log(isElementFind(num))
+
+// 8
 // function numThree(arr){
 //     let sum = 0;
 //     let count = 0;
 //     arr.forEach(num =>{
-//         if(num >= 100 && num <= 999){
+//         if(num >= 100 && num < 1000 && num %2 !== 0 ){
 //             sum += num;
 //             count++;
 //         }
 //     })
-//     if(count === 0){
-//         return 0;
-//     }
-//     return sum / count;
+//     return count > 0? sum/count : 0;
 // }
-// console.log(numThree(num));
+// console.log(numThree([123, 456, 789, 101, 102, 103]))
 
-// 7
-// let numbers = [1, 2, 3, 4, 5];
-// let transformed = transformArray(numbers);
+// 9
 // function transformArray(arr) {
-//     return arr.map(num => {
-//         if (num % 2 === 0) {
-//             return num ** 2;
+//     return arr.map(number => {
+//         if (number % 2 === 0) {
+//             return Math.pow(number, 2);
 //         } else {
-//             return num ** 3;
+//             return Math.pow(number, 3);
 //         }
 //     });
 // }
-// console.log(transformed);
-
-// 8
-// let fruits = ['gilos', 'o`rik', 'shaftoli', 'olma', 'olcha'];
-// let lastLetter = getLastLetters(fruits);
-// function getLastLetters(arr){
-//     return arr.map(str => str.slice(-1))
-// }
-// console.log(lastLetter)
-
-// 9
-// let words = ['hard', 'pig', 'eat', 'dog'];
-// let lastLetter = getLastLetters(words);
-// function getLastLetters(arr){
-//     return arr.filter(str => str.length < 4);
-// }
-// console.log(lastLetter)
+// console.log(transformArray([1, 2, 3, 4, 5, 7, 8]));
 
 // 10
-// let user = [
-//     {name:'Jhon', age:5},
-//     {name:'Doe', age:30},
-//     {name:'Alex', age:50},
-// ]
-// function userAbout(arr){
-//     return arr.some(person => person.age > 18)
+// function getLastCharacters(string) {
+//     return string.map(str => str[str.length - 1]);
 // }
-// console.log(userAbout(user))
+// console.log(getLastCharacters(['apple', 'banana', 'cherry']));
 
 // 11
-// let names = ['Zafarjon', 'Ibrohimjon', 'Murodjon', 'Bekzodjon'];
+// function filterShortStrings(string) {
+//     return string.filter(str => str.length < 4);
+// }
+// console.log(filterShortStrings(['apple', 'cat', 'banana', 'dog', 'fish']));
+
+// 12
+// let user = [
+//     {name:'John', age:5},
+//     {name:'Doe', age:20},
+//     {name:'Alex', age:50}
+// ]
+// function bigAge(obj){
+//     return obj.some(obj =>obj.age > 18)
+// }
+// console.log(bigAge(user))
+
+// 13
+// let names = ['Zafarjon', 'Ibrohimjon', 'Murodjon', 'Bekzodbek'];
 // function endWithJon(names) {
 //     return names.every(name => name.endsWith("jon"));
 // }
 // console.log(endWithJon(names))
 
-// 12
-// let numbers = [12, 75, 151, 400, 215, 678, 985];
-// function uchHonaliSonlar(arr) {
-//     return arr.find(num => num >= 100 && num <= 999 && num % 5 === 0);
+// 14
+// function findThreeDigitMultipleOfFive(array) {
+//     return array.find(number => number >= 100 && number < 1000 && number % 5 === 0);
 // }
-// console.log(uchHonaliSonlar(numbers));
+// console.log(findThreeDigitMultipleOfFive([50, 150, 200, 305, 450]));
 
-// 13
-// let names = ['Zafarbek', 'Jasurbek', 'Aziz'];
-// function findBek(names){
+// 15
+// function findIndexWithBek(names) {
 //     return names.findIndex(name => name.includes('bek'));
 // }
-// console.log(findBek(names))
+// console.log(findIndexWithBek(['Zafarjon', 'Boburbek']));   
 
 // OBJECT METHODIGA OID MASALALAR
 // 1
